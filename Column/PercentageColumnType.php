f99569fd43f84c83a05d0b39c497e7bfed54d69c
Florian Meyer <florian.meyer@project-engineers.de>
2019-11-14 11:28:35 +0100

    Added percentage column type

---
+++ Column/PercentageColumnType.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+/*
+ * This file is part of the Stinger Soft AgGrid package.
+ *
+ * (c) Oliver Kotte <oliver.kotte@stinger-soft.net>
+ * (c) Florian Meyer <florian.meyer@stinger-soft.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace StingerSoft\AggridBundle\Column;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Renders a percentage formatted value in a column using PHPs number format capabilities.
+ *
+ * @see NumberFormatterColumnType for formatting numbers in a more generic way
+ */
+class PercentageColumnType extends AbstractColumnType {
+
+	/**
+	 * @inheritdoc
+	 *
+	 * @see AbstractColumnType::configureOptions()
+	 */
+	public function configureOptions(OptionsResolver $resolver, array $tableOptions = array()): void {
+		$resolver->setDefault('number_formatter_style', \NumberFormatter::PERCENT);
+		$resolver->setDefault('filterValueGetter', 'PercentageValueGetter');
+
+		$resolver->setDefault('format_null', true);
+		$resolver->setAllowedTypes('format_null', 'boolean');
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getParent(): ?string {
+		return NumberFormatterColumnType::class;
+	}
+
+}

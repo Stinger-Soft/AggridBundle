8f452cfef7ea048f41ee6c6bcc66493f6e8c444e
Florian Meyer <Viburnum@users.noreply.github.com>
2020-03-05 14:46:03 +0100

    Fixed: deep find will return null if the given object is undefined

--- Resources/public/scripts/StingerSoftAggridGetter.js
+++ Resources/public/scripts/StingerSoftAggridGetter.js
@@ -43,13 +43,16 @@
      */
 
     StingerSoftAggrid.Getter.deepFind = function (obj, path) {
+        if(obj === undefined) {
+            return null;
+        }
         var paths = path.split('.')
             , current = obj
             , i;
 
         for (i = 0; i < paths.length; ++i) {
             if (current[paths[i]] == undefined) {
-                return undefined;
+                return null;
             } else {
                 current = current[paths[i]];
             }
